b7bb8942cb0e87a6aeea1fa1c57ef4ea9465c0ac
ArchithaDevanuru <160098924+ArchithaD@users.noreply.github.com>
2025-07-09 12:23:18 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,16 +1,16 @@
 import pandas as pd
-from sklearn.neighbors import KNeighborsClassifier
 import joblib
 
-# Load data
-df = pd.read_csv('data/iris.data')
-X = df.drop('Iris-setosa', axis=1)
-y = df['Iris-setosa']
+# Load model
+model = joblib.load('model/model.pkl')
 
-# Train model
-model = KNeighborsClassifier()
-model.fit(X, y)
+# Sample patient
+sample = pd.DataFrame({
+   '5.1':[5.0],
+   '3.5':[3.0],
+   '1.4':[1.4],
+   '0.2':[0.2],
+})
 
-# Save model
-joblib.dump(model, 'model/model.pkl')
-print(" iris model trained.")
+prediction = model.predict(sample)[0]
+print(f" Prediction: {prediction}")
